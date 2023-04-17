53853005d4a311687b39d49e74622e1d9ef7c665
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-17 09:55:25 +0530

    Did doc

---
+++ Doctor.sql
@@ -0,0 +1,24 @@
+
+
+INSERT ALL
+    INTO DOCTOR VALUES('455268921', 'Jennifer', 'K', 'Watson', 'Cardiology', 15, 'Family')
+    INTO DOCTOR VALUES('278139725', 'Stephen', 'H', 'Hales', 'Orthopedics', 12, 'Family')
+    INTO DOCTOR VALUES('395071862', 'Greyson', 'O', 'Wallace', 'Neurology', 4, 'Non Family')
+    INTO DOCTOR VALUES('455268981', 'Emerson', 'A', 'Borg', 'Neurology', 23, 'Family')
+    INTO DOCTOR VALUES('875569923', 'Charles', 'M', 'John', 'Gynaecology', 32, 'Non Family')
+    INTO DOCTOR VALUES('875584923', 'Dean', 'Q', 'English', 'Neurology', 10, 'Non Family')
+    INTO DOCTOR VALUES('241157321', 'Daniel', 'C', 'Smith', 'Neuropsychiatrist', 15, 'Family')
+    INTO DOCTOR VALUES('671298627', 'Susane', 'L', 'Willams', 'Orthopedics', 3, 'Family')
+    INTO DOCTOR VALUES('543893122', 'Fancis', 'E', 'Morgan', 'Orthopedics', 6, 'Non Family')
+    INTO DOCTOR VALUES('566329101', 'Albus', 'F', 'Dumbledore', 'Neurology', 10, 'Family')
+    INTO DOCTOR VALUES('543899122', 'Fancis', 'P', 'Morgan', 'Cardiology', 4, 'Non Family')
+    INTO DOCTOR VALUES('617489235', 'Emily', 'B', 'Devilliers', 'Gynaecology', 6, 'Family')
+    INTO DOCTOR VALUES('820673914', 'Daniel', 'N', 'Johnson', 'Orthopedics', 3, 'Non Family')
+    INTO DOCTOR VALUES('569329101', 'David', 'R', 'Wood', 'Cardiology', 8, 'Non Family')
+    INTO DOCTOR VALUES('934826507', 'Michael', 'G', 'Brown', 'Cardiology', 2, 'Non Family')
+    INTO DOCTOR VALUES('582176049', 'Ava', 'J', 'Garcia', 'Neurology', 15, 'Family')
+    INTO DOCTOR VALUES('712844569', 'George', 'D', 'Starc', 'Gynaecology', 20, 'Family')
+    INTO DOCTOR VALUES('476502198', 'Ivana', 'I', 'Johnson', 'Cardiology', 7, 'Non Family')
+SELECT 1 
+FROM DUAL;
+
