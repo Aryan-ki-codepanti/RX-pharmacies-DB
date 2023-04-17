995e9cac577f405ed24bb95f9bb517b810aad3d1
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-17 14:17:06 +0530

    Added patient

---
+++ Patient.sql
@@ -0,0 +1,25 @@
+
+INSERT ALL
+    INTO PATIENT VALUES('627950000', 'Sumit', 'S',	'Chauhan', TO_DATE('18-12-2001', 'DD-MM-YYYY'),	'A/12 Tagore Garden New Delhi', '455268921')
+    INTO PATIENT VALUES('218260000', 'Neeta', NULL,'Jaiswal', TO_DATE('03-09-1998', 'DD-MM-YYYY'), 'Z-23 Preet Vihar New Delhi', '278139725')
+    INTO PATIENT VALUES('102710000', 'Keshav', NULL,'Oberoi', TO_DATE('10-10-2000', 'DD-MM-YYYY'),	'H-52 CGO Complex New Delhi', '455268981')
+    INTO PATIENT VALUES('202830000', 'Himanshi', NULL,'Arora', TO_DATE('22-01-1999', 'DD-MM-YYYY'), '88/12 B Block Hari Nagar New Delhi', '241157321')
+    INTO PATIENT VALUES('982530000', 'Geet', NULL, 'Kaur', TO_DATE('16-11-2002', 'DD-MM-YYYY'), 'E-81 Sonipat Haryana', '671298627')
+    INTO PATIENT VALUES('127830000', 'Bhupesh', 'S', 'Kumar', TO_DATE('17-04-1990', 'DD-MM-YYYY'),	'23/19 E Block Keshavpuram New Delhi', '566329101')
+    INTO PATIENT VALUES('725690000', 'Dheeraj', 'R', 'Anand', TO_DATE('08-12-2003', 'DD-MM-YYYY'),	'12/15 J Block Paschim Puri New Delhi', '617489235')
+    INTO PATIENT VALUES('123450000', 'Yasmine', NULL, 'Desai', TO_DATE('24-05-2010', 'DD-MM-YYYY'), '9/11 Karol Bagh New Delhi', '582176049')
+    INTO PATIENT VALUES('392010000', 'Jyoti',  NULL,'Arora', TO_DATE('20-08-2005', 'DD-MM-YYYY'), 'A-283 Kirti Nagar New Delhi', '712844569')
+    INTO PATIENT VALUES('291730000', 'Ramesh', 'S', 'Narayan', TO_DATE('05-05-2005', 'DD-MM-YYYY'), 'J-128 Laxmi Nagar New Delhi', '455268921')
+    INTO PATIENT VALUES('228730000', 'Shivani', NULL, 'Bedi', TO_DATE('29-10-2008', 'DD-MM-YYYY'),	'O-52 Dwarka Sector 11 New Delhi', '278139725')
+    INTO PATIENT VALUES('654370000', 'Tanishq', 'S', 'Dhami', TO_DATE('16-02-2012', 'DD-MM-YYYY'),	'7/22 Rohini Sector 7 New Delhi', '455268981')
+    INTO PATIENT VALUES('286530000', 'Rahul', NULL,'Gauri', TO_DATE('21-09-2009', 'DD-MM-YYYY'), 'C-189 Pitampura New Delhi', '241157321')
+    INTO PATIENT VALUES('453820000', 'Nitin', 'K', 'Gupta', TO_DATE('09-05-2001', 'DD-MM-YYYY'), 'HN-21 Ludhiana Punjab', '671298627')
+    INTO PATIENT VALUES('275380000', 'Ashish', NULL,'Oberoi', TO_DATE('30-07-2010', 'DD-MM-YYYY'), '45/22 N Block Punjabi Bagh New Delhi', '566329101')
+    INTO PATIENT VALUES('285390000', 'Driti', NULL, 'Gupta', TO_DATE('20-12-2000', 'DD-MM-YYYY'), '8/11 B Block Shakurpur New Delhi', '617489235')
+    INTO PATIENT VALUES('261960000', 'Dev', NULL, 'Agarwal', TO_DATE('21-04-2007', 'DD-MM-YYYY'), 'A-66 Bhikaji Kama Place New Delhi', '582176049')
+    INTO PATIENT VALUES('982630000', 'Alok', NULL, 'Khurana', TO_DATE('10-10-2008', 'DD-MM-YYYY'),	'22/10 M Block Raghubir Nagar New Delhi', '712844569')
+    INTO PATIENT VALUES('876770000', 'Lakshita', NULL, 'Oberoi', TO_DATE('07-06-2015', 'DD-MM-YYYY'), 'J-12 Karkardooma New Delhi', '455268981')
+    INTO PATIENT VALUES('189270000', 'Prateek', 'S', 'Maan', TO_DATE('01-07-2016', 'DD-MM-YYYY'), '90/11 L Block Nawada New Delhi', '241157321')
+
+SELECT 1
+FROM DUAL;
