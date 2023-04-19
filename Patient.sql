f59b26cc551350d0181b3ffaf920ff498a2655dc
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-19 10:25:37 +0530

    Did changes acc to latest state

--- Patient.sql
+++ Patient.sql
@@ -4,7 +4,7 @@ INSERT ALL
     INTO PATIENT VALUES('218260000', 'Neeta', NULL,'Jaiswal', TO_DATE('03-09-1998', 'DD-MM-YYYY'), 'Z-23 Preet Vihar New Delhi', '278139725')
     INTO PATIENT VALUES('102710000', 'Keshav', NULL,'Oberoi', TO_DATE('10-10-2000', 'DD-MM-YYYY'),	'H-52 CGO Complex New Delhi', '455268981')
     INTO PATIENT VALUES('202830000', 'Himanshi', NULL,'Arora', TO_DATE('22-01-1999', 'DD-MM-YYYY'), '88/12 B Block Hari Nagar New Delhi', '241157321')
-    INTO PATIENT VALUES('982530000', 'Geet', NULL, 'Kaur', TO_DATE('16-11-2002', 'DD-MM-YYYY'), 'E-81 Sonipat Haryana', '671298627')
+    INTO PATIENT VALUES('982530000', 'Geet', NULL, 'Kaur', TO_DATE('16-11-2012', 'DD-MM-YYYY'), 'E-81 Sonipat Haryana', '671298627')
     INTO PATIENT VALUES('127830000', 'Bhupesh', 'S', 'Kumar', TO_DATE('17-04-1990', 'DD-MM-YYYY'),	'23/19 E Block Keshavpuram New Delhi', '566329101')
     INTO PATIENT VALUES('725690000', 'Dheeraj', 'R', 'Anand', TO_DATE('08-12-2003', 'DD-MM-YYYY'),	'12/15 J Block Paschim Puri New Delhi', '617489235')
     INTO PATIENT VALUES('123450000', 'Yasmine', NULL, 'Desai', TO_DATE('24-05-2010', 'DD-MM-YYYY'), '9/11 Karol Bagh New Delhi', '582176049')
@@ -19,7 +19,7 @@ INSERT ALL
     INTO PATIENT VALUES('261960000', 'Dev', NULL, 'Agarwal', TO_DATE('21-04-2007', 'DD-MM-YYYY'), 'A-66 Bhikaji Kama Place New Delhi', '582176049')
     INTO PATIENT VALUES('982630000', 'Alok', NULL, 'Khurana', TO_DATE('10-10-2008', 'DD-MM-YYYY'),	'22/10 M Block Raghubir Nagar New Delhi', '712844569')
     INTO PATIENT VALUES('876770000', 'Lakshita', NULL, 'Oberoi', TO_DATE('07-06-2015', 'DD-MM-YYYY'), 'J-12 Karkardooma New Delhi', '455268981')
-    INTO PATIENT VALUES('189270000', 'Prateek', 'S', 'Maan', TO_DATE('01-07-2016', 'DD-MM-YYYY'), '90/11 L Block Nawada New Delhi', '241157321')
+    INTO PATIENT VALUES('189270000', 'Prateek', 'S', 'Maan', TO_DATE('01-07-2012', 'DD-MM-YYYY'), '90/11 L Block Nawada New Delhi', '241157321')
 
 SELECT 1
 FROM DUAL;
